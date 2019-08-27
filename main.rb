e5cda5c20f16a000d4eae22d8b438f640b7c62cf
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-08-27 13:10:57 -0400

    added bubble sort function

---
+++ main.rb
@@ -0,0 +1,42 @@
+
+array = [ 4 ,3 ,78 ,2 ,0 ,2 ]
+
+
+
+def bubble_sort(array)
+  isFinished = false
+
+  while isFinished == false
+    isFinished = true
+    counter = 1
+
+    # loop from 0 to 5
+    for num in 0...array.length - counter
+      if array[num] > array[num+1]
+        #swap numbers
+        aux = array[num]
+        array[num] = array[num+1]
+        array[num+1] = aux
+        isFinished = false
+      end
+    end
+
+    counter += 1
+
+    print array
+    puts "\n"
+
+  end
+
+end
+
+
+bubble_sort(array)
+
+
+
+
+def bubble_sort_by(array) do |left, right|
+
+
+end
