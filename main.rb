6053f8b18602abed9c64429b563d25c41ff86e14
hugopassos <hugo.passos@hotmail.com>
2019-08-27 14:54:01 -0300

    Added bubble_sort_by function

--- main.rb
+++ main.rb
@@ -27,16 +27,35 @@ def bubble_sort(array)
     puts "\n"
 
   end
-
 end
-
 
 bubble_sort(array)
 
+def bubble_sort_by(array)
+  isFinished = false
 
+  while isFinished == false
+    isFinished = true
+    counter = 1
 
+    # loop from 0 to 5
+    for num in 0...array.length - counter
+      if yield(array[num], array[num+1]) > 0
+        #swap numbers
+        aux = array[num]
+        array[num] = array[num+1]
+        array[num+1] = aux
+        isFinished = false
+      end
+    end
 
-def bubble_sort_by(array) do |left, right|
+    counter += 1
+    print array
+    puts "\n"
 
+  end
+end
 
+bubble_sort_by(["hi","hello","hey", "ola", "holaaaa", "oi"]) do |left,right|
+  left.length - right.length
 end
